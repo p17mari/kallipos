e035a204c56e8ded32cc385503a6aef395f65eb2
p17mari <43947917+p17mari@users.noreply.github.com>
2022-11-17 18:51:40 +0200

    Update make-latex.sh

--- make-latex.sh
+++ make-latex.sh
@@ -13,7 +13,7 @@ pandoc text/epi.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level
 
 for filename in text/ch*.txt; do
 [ -e "$filename" ] || continue
- pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc--lua-filter=student-contribution.lua --to markdown |pandoc --lua-filter=epigraph.lua --to markdown |  pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex" 
+ pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=student-contribution.lua --to markdown |pandoc --lua-filter=epigraph.lua --to markdown |  pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex" 
 done 
 
 # sed -i '' 's+Figure+Εικόνα+g' ./latex/ch0*
