d1becf98caa3dd43d2997590dd6e0fc05d10bbea
p17mari <43947917+p17mari@users.noreply.github.com>
2022-11-17 17:58:15 +0200

    Update student-contribution.lua

--- student-contribution.lua
+++ student-contribution.lua
@@ -1,23 +1,12 @@
-function studentfont()
-  --[[
-  
-  if img.classes:find('student',1) 
-  stringify = pandoc.utils.stringify
-  if #el.content == 1 then
-    local ft = stringify(el.content[1]) 
-    if string.sub(ft,1,4) == "fig:" then
-        local fn = string.sub(ft,5)
-        --print(fn)
-        local f = io.open("figures/" .. fn .. ".md", 'r')
-  change forn here -->  ObjectFont = Fonts.studentfont
-  
-  maybe before printing the local add border here
-    add student author credentials in upper left corner.
-    add student contribution icon on the left corner
-        local doc = pandoc.read(f:read('*a'))]
-  f:close()
-  local print("No student contributions in this chapter,")
-  return pandoc.Note("*@" .. ft .. " " .. title .. " (" .. license_text .. ")")
-   --]]
-  
-  end
+function Image(img)
+    if img.classes:find('student-contribution',1) then
+      local f = io.open("extras/" .. img.src, 'r')
+      local doc = pandoc.read(f:read('*a'))
+      f:close()
+      local caption = pandoc.utils.stringify(doc.meta.caption) 
+      local name = pandoc.utils.stringify(doc.meta.name)
+      local am = pandoc.utils.stringify(doc.meta.id)
+      local content = "> _" .. caption .. "_  \n>" .. "Ονοματεπωνυμο Φοιτητη:" .. name .. "\nAριθμος Mητρωου:" .. am
+      return pandoc.RawInline('markdown',content)
+    end
+end
