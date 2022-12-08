4c009584ff6fc5b61fe6ddfeb594a9b03d3e6cc4
Argiro Marioli <argiromar7@gmail.com>
2022-12-08 21:41:43 +0200

    Update and rename studentcontribution.lua to student.lua

--- student.lua
+++ student.lua
@@ -1,17 +1,15 @@
-
 function Image(img)
       local stringify = pandoc.utils.stringify
-      if img.classes:find('studentcontribution',1) then
+      if img.classes:find('student',1) then
         local fn = img.src
         --print(fn)
-        local f = io.open("contribution/" .. fn, 'r')
+        local f = io.open("contribution/" .. fn .. ".md", 'r')
         local doc = pandoc.read(f:read('*a'))
         f:close()
         local figid = string.sub(fn,1,string.len(fn)-3)
-        --local src = stringify(doc.meta.image_url) or "src has not been set" this is for an image I will uncomment if I add one
-        src = "." .. src
+        local src = stringify(doc.meta.image_url) or "src has not been set"
+        src = ".." .. src
         local caption = stringify(doc.meta.caption) 
         return pandoc.Image(caption,src,nil,"fig:" .. figid)
       end
 end
-
