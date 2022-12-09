52e49ea0e1255b49e22b6b25c5709e5da2af661a
Argiro Marioli <argiromar7@gmail.com>
2022-12-09 22:14:23 +0200

    Update student.lua

--- student.lua
+++ student.lua
@@ -1,15 +1,11 @@
 function Image(img)
-      local stringify = pandoc.utils.stringify
       if img.classes:find('student',1) then
-        local fn = img.src
-        --print(fn)
-        local f = io.open("contribution/" .. fn .. ".md", 'r')
+        local f = io.open("contribution/" .. img.src, 'r')
         local doc = pandoc.read(f:read('*a'))
         f:close()
-        local figid = string.sub(fn,1,string.len(fn)-3)
-        local src = stringify(doc.meta.image_url) or "src has not been set"
-        src = ".." .. src
-        local caption = stringify(doc.meta.caption) 
-        return pandoc.Image(caption,src,nil,"fig:" .. figid)
+        local caption = pandoc.utils.stringify(doc.meta.caption) or "Caption has not been set"
+        local author = pandoc.utils.stringify(doc.meta.author) or "Author has not been set"
+        local contribution = "> " .. caption .. " " .. author
+        return pandoc.RawInline('markdown', student)
       end
 end
