1ba36249de13c5018a5e9f7c72e81ca2ec2e7f83
Argiro Marioli <argiromar7@gmail.com>
2022-12-09 22:58:09 +0200

    Update student.lua

--- student.lua
+++ student.lua
@@ -1,6 +1,6 @@
 function Image(img)
       if img.classes:find('student',1) then
-        local f = io.open("contribution/" .. img.src, 'r')
+        local f = io.open("contributions/" .. img.src, 'r')
         local doc = pandoc.read(f:read('*a'))
         f:close()
         local caption = pandoc.utils.stringify(doc.meta.caption) or "Caption has not been set"
